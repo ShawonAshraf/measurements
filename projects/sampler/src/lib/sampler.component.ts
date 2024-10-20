8db78f337a97232cf7851201a8f4de76f2380693
ShawonAshraf <7804029+ShawonAshraf@users.noreply.github.com>
2024-10-20 22:58:53 +0200

    init boilerplate

---
+++ projects/sampler/src/lib/sampler.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-sampler',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      sampler works!
+    </p>
+  `,
+  styles: ``
+})
+export class SamplerComponent {
+
+}
---
+++ projects/sampler/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of sampler
+ */
+
+// export * from './lib/sampler.service';
+// export * from './lib/sampler.component';
+
+import init from './lib/sampler-lib/pkg';
+export { get_factorial } from './lib/sampler-lib/pkg'
+export { init as initSampler };
