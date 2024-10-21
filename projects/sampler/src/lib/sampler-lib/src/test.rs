c67ba73186f9666b15067e75351d0f0fbdb6bc2d
ShawonAshraf <7804029+ShawonAshraf@users.noreply.github.com>
2024-10-21 08:18:51 +0200

    updated tests

--- projects/sampler/src/lib/sampler-lib/src/test.rs
+++ projects/sampler/src/lib/sampler-lib/src/test.rs
@@ -6,21 +6,21 @@ mod test {
 
     #[test]
     fn test_get_interval_start() {
-        let dt = Utc.ymd(2021, 1, 1).and_hms(0, 0, 0);
+        let dt = Utc.with_ymd_and_hms(2021, 1, 1, 0, 0, 0).unwrap();
         let start = get_interval_start(dt);
-        assert_eq!(start, Utc.ymd(2021, 1, 1).and_hms(0, 0, 0));
+        assert_eq!(start, Utc.with_ymd_and_hms(2021, 1, 1, 0, 0, 0).unwrap());
     }
 
     #[test]
     fn test_str_to_utc() {
         let ts = "2021-01-01T00:00:00".to_string();
         let dt = str_to_utc(&ts);
-        assert_eq!(dt, Utc.ymd(2021, 1, 1).and_hms(0, 0, 0));
+        assert_eq!(dt, Utc.with_ymd_and_hms(2021, 1, 1, 0, 0, 0).unwrap());
     }
 
     #[test]
     fn test_utc_to_str() {
-        let dt = Utc.ymd(2021, 1, 1).and_hms(0, 0, 0);
+        let dt = Utc.with_ymd_and_hms(2021, 1, 1, 0, 0, 0).unwrap();
         let ts = utc_to_str(&dt);
         assert_eq!(ts, "2021-01-01T00:00:00");
     }
@@ -86,5 +86,57 @@ mod test {
 
         let latest = find_latest_in_interval(&test_data);
         assert_eq!(latest.len(), 2);
+    }
+
+    #[test]
+    fn test_sample() {
+        let measurement1 = Measurement {
+            timestamp: String::from("2017-01-03T10:04:45"),
+            measurement_type: String::from("TEMP"),
+            value: 35.79,
+        };
+        let measurement2 = Measurement {
+            timestamp: String::from("2017-01-03T10:01:18"),
+            measurement_type: String::from("SPO2"),
+            value: 98.78,
+        };
+        let measurement3 = Measurement {
+            timestamp: String::from("2017-01-03T10:09:07"),
+            measurement_type: String::from("TEMP"),
+            value: 35.01,
+        };
+        let measurement4 = Measurement {
+            timestamp: String::from("2017-01-03T10:03:34"),
+            measurement_type: String::from("SPO2"),
+            value: 96.49,
+        };
+        let measurement5 = Measurement {
+            timestamp: String::from("2017-01-03T10:02:01"),
+            measurement_type: String::from("TEMP"),
+            value: 35.82,
+        };
+        let measurement6 = Measurement {
+            timestamp: String::from("2017-01-03T10:05:00"),
+            measurement_type: String::from("SPO2"),
+            value: 97.17,
+        };
+        let measurement7 = Measurement {
+            timestamp: String::from("2017-01-03T10:05:01"),
+            measurement_type: String::from("SPO2"),
+            value: 95.08,
+        };
+
+        let test_data: Vec<Measurement> = vec![
+            measurement1,
+            measurement2,
+            measurement3,
+            measurement4,
+            measurement5,
+            measurement6,
+            measurement7,
+        ];
+
+        let results = sample(&test_data);
+        assert_eq!(results.len(), 4);
     }
 }
